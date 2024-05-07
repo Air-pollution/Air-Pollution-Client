f078e5fb9dd7cbf40d9c5862ecc6bcb29e61289b
Tạ Đức Bảo <146197243+DucBaoUIT@users.noreply.github.com>
2024-05-07 13:07:06 +0700

    Update LineChartRechart.jsx

--- components/LineChartRechart.jsx
+++ components/LineChartRechart.jsx
@@ -97,12 +97,24 @@ import Navbar from './NavBar';
 import { StyleSheet } from 'react-native';
 
 const LineChartRechart = () => {
+    const [testdata, setTestData] = useState([
+        { name: '1', temperature: 25, humidity: 50 ,pressure: 1010, co: 300 },
+        { name: '2', temperature: 24, humidity: 50 ,pressure: 1010, co: 300 },
+        { name: '3', temperature: 23, humidity: 50 ,pressure: 1010, co: 300 },
+        { name: '4', temperature: 22, humidity: 50 ,pressure: 1010, co: 300 },
+        { name: '5', temperature: 21, humidity: 50 ,pressure: 1010, co: 300 },
+        { name: '6', temperature: 20, humidity: 50 ,pressure: 1010, co: 300 },
+        { name: '7', temperature: 19, humidity: 50 ,pressure: 1010, co: 300 },
+        { name: '8', temperature: 18, humidity: 50 ,pressure: 1010, co: 300 },
+        { name: '9', temperature: 17, humidity: 50 ,pressure: 1010, co: 300 },
+        { name: '10', temperature: 16, humidity: 50 ,pressure: 1010, co: 300 },
+    ]);
     const [data, setData] = useState([
         { name: '1', temperature: 25 },
         { name: '2', temperature: 24 },
         { name: '3', temperature: 23 },
         { name: '4', temperature: 22 },
-        { name: '5', temperature: 21 },
+        { name: '5', temperature: 21 }
         { name: '6', temperature: 20 },
         { name: '7', temperature: 19 },
         { name: '8', temperature: 18 },
@@ -152,14 +164,16 @@ const LineChartRechart = () => {
             <View style={styles.container}>
                 <View style={styles.chartContainer}>
 
-                    <LineChart width={600} height={400} margin={{ top: 5, right: 20, left: 10, bottom: 5 }}>
+                    <LineChart data={testdata} width={600} height={400} margin={{ top: 5, right: 20, left: 10, bottom: 5 }}>
                         <XAxis dataKey="name" />
                         <Tooltip />
                         <CartesianGrid stroke="#f5f5f5" />
-                        <Line data={data} type="monotone" dataKey="temperature" stroke="#e26a00" />
-                        <Line data={dataHumid} type="monotone" dataKey="humidity" stroke="#1E88E5" />
-                        <Line data={dataPressure} type="monotone" dataKey="pressure" stroke="#FF5722" />
-                        <Line data={dataCo} type="monotone" dataKey="co" stroke="#78909C" />
+                        <Line  type="monotone" dataKey="temperature" stroke="#e26a00" yAxisId={0} />
+                        <Line  type="monotone" dataKey="humidity" stroke="#e26a00" yAxisId={0} />
+                        <Line  type="monotone" dataKey="pressure" stroke="#e26a00" yAxisId={0} />
+                        <Line  type="monotone" dataKey="co" stroke="#e26a00" yAxisId={0} />
+
+
                     </LineChart>
                 </View>
             </View>
